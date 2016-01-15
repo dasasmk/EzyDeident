39782ada314d982756bb4da33f4c22af9ecb64d7
dasasmk <ephsmk@nus.edu.sg>
2016-01-15 11:26:07 +0800

    Add Manual

--- run_from_github.R
+++ run_from_github.R
@@ -1,4 +1,4 @@
 source("https://raw.githubusercontent.com/dasasmk/EzyDeident/master/setup.R")
 library(shiny)
-runGitHub("EzyDeident", "dasasmk") 
+runGitHub("EzyDeident", "dasasmk",launch.browser=TRUE,port=9999) 
           
