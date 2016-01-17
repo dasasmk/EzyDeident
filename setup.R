f119db9eeaffec3a4f675a9dc3f76fdc3168f507
dasasmk <ephsmk@nus.edu.sg>
2016-01-17 14:09:58 +0800

    Add Cran Repo

--- setup.R
+++ setup.R
@@ -3,103 +3,103 @@ packs <- installed.packages()
 packs <- names(packs[,'Package'])
 
 if(!any(packs%in%"PKI")){
-  install.packages("PKI")  
+  install.packages("PKI", repos="http://cran.us.r-project.org")   
 }
 
 if(!any(packs%in%"R6")){
-  install.packages("R6")  
+  install.packages("R6", repos="http://cran.us.r-project.org")   
 }
 
 if(!any(packs%in%"RCurl")){
-  install.packages("RCurl")  
+  install.packages("RCurl", repos="http://cran.us.r-project.org")    
 }
 
 if(!any(packs%in%"Rcpp")){
-  install.packages("Rcpp")  
+  install.packages("Rcpp", repos="http://cran.us.r-project.org")    
 }
 
 if(!any(packs%in%"base64enc")){
-  install.packages("base64enc")  
+  install.packages("base64enc", repos="http://cran.us.r-project.org")    
 }
 
 if(!any(packs%in%"bitops")){
-  install.packages("bitops")  
+  install.packages("bitops", repos="http://cran.us.r-project.org")    
 }
 
 if(!any(packs%in%"caTools")){
-  install.packages("caTools")  
+  install.packages("caTools", repos="http://cran.us.r-project.org")    
 }
 
 if(!any(packs%in%"digest")){
-  install.packages("digest")  
+  install.packages("digest", repos="http://cran.us.r-project.org")    
 }
 
 if(!any(packs%in%"evaluate")){
-  install.packages("evaluate")  
+  install.packages("evaluate", repos="http://cran.us.r-project.org")    
 }
 
 if(!any(packs%in%"formatR")){
-  install.packages("formatR")  
+  install.packages("formatR", repos="http://cran.us.r-project.org")   
 }
 
 if(!any(packs%in%"highr")){
-  install.packages("highr")  
+  install.packages("highr", repos="http://cran.us.r-project.org")    
 }
 
 if(!any(packs%in%"htmltools")){
-  install.packages("htmltools")  
+  install.packages("htmltools", repos="http://cran.us.r-project.org")    
 }
 
 if(!any(packs%in%"httpuv")){
-  install.packages("httpuv")  
+  install.packages("httpuv", repos="http://cran.us.r-project.org")    
 }
 
 if(!any(packs%in%"jsonlite")){
-  install.packages("jsonlite")  
+  install.packages("jsonlite", repos="http://cran.us.r-project.org")    
 }
 
 if(!any(packs%in%"knitr")){
-  install.packages("knitr")  
+  install.packages("knitr", repos="http://cran.us.r-project.org")    
 }
 
 if(!any(packs%in%"magrittr")){
-  install.packages("magrittr")  
+  install.packages("magrittr", repos="http://cran.us.r-project.org")   
 }
 
 if(!any(packs%in%"markdown")){
-  install.packages("markdown")  
+  install.packages("markdown", repos="http://cran.us.r-project.org")   
 }
 
 if(!any(packs%in%"mime")){
-  install.packages("mime")  
+  install.packages("mime", repos="http://cran.us.r-project.org")    
 }
 
 if(!any(packs%in%"plyr")){
-  install.packages("plyr")  
+  install.packages("plyr", repos="http://cran.us.r-project.org")    
 }
 
 if(!any(packs%in%"rmarkdown")){
-  install.packages("rmarkdown")  
+  install.packages("rmarkdown", repos="http://cran.us.r-project.org")    
 }
 
 if(!any(packs%in%"shiny")){
-  install.packages("shiny")  
+  install.packages("shiny", repos="http://cran.us.r-project.org")   
 }
 
 if(!any(packs%in%"stringi")){
-  install.packages("stringi")  
+  install.packages("stringi", repos="http://cran.us.r-project.org")    
 }
 
 if(!any(packs%in%"stringr")){
-  install.packages("stringr")  
+  install.packages("stringr", repos="http://cran.us.r-project.org")    
 }
 
 if(!any(packs%in%"xtable")){
-  install.packages("xtable")  
+  install.packages("xtable", repos="http://cran.us.r-project.org")    
 }
 
 if(!any(packs%in%"yaml")){
-  install.packages("yaml")  
+  install.packages("yaml", repos="http://cran.us.r-project.org")    
 }
 
 if(!any(packs%in%"pander")){
@@ -107,7 +107,7 @@ if(!any(packs%in%"pander")){
 }
 
 if(!any(packs%in%"downloader")){
-  install.packages("downloader")  
+  install.packages("downloader", repos="http://cran.us.r-project.org")   
 }
 
 
